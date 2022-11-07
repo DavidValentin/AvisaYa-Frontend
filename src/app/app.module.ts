1572914efb2236e3261e567073b6f532e866e3d1
MarcosValdez <marcos.valdez333@hotmail.com>
2022-11-06 22:37:24 -0500

    page 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NotFoundComponent } from './layout/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
