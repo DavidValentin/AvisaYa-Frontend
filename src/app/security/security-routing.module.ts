c22c0dfa5e1460d135b9ea23790d042196ea0baa
DavidValentin <dfvaler@gmail.com>
2022-11-11 19:57:04 -0500

    feat: profile routing added

--- src/app/security/security-routing.module.ts
+++ src/app/security/security-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './pages/profile/profile.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
     pathMatch: 'full',
   },
 ];
