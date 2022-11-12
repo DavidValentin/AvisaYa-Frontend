3695791129e509a004cc23abd7b2fb4b64e8cbb0
MarcosValdez <marcos.valdez333@hotmail.com>
2022-11-11 23:58:03 -0500

    fix: se modifico la cabecera

--- src/app/security/security-routing.module.ts
+++ src/app/security/security-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { RegisterComponent } from './pages/register/register.component';
 
@@ -12,6 +13,11 @@ const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
     pathMatch: 'full',
   },
 ];
