e3c5c96c3e7b07c42f516d4f8ba2789b0fa13d7e
DavidValentin <dfvaler@gmail.com>
2022-12-24 09:34:31 -0500

    feat testing login delito component

--- src/app/security/pages/login/login.component.spec.ts
+++ src/app/security/pages/login/login.component.spec.ts
@@ -1,14 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpResponse,
+} from '@angular/common/http';
+
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
+      imports: [HttpClientTestingModule, FormsModule, ReactiveFormsModule],
     }).compileComponents();
   });
 
@@ -16,6 +32,8 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should create', () => {
