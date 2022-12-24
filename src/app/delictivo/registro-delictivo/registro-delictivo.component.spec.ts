3919fbf8470c8e9decf723449b9569c4f0fc7fb4
DavidValentin <dfvaler@gmail.com>
2022-12-23 19:20:15 -0500

    test: testing using http added

--- src/app/delictivo/registro-delictivo/registro-delictivo.component.spec.ts
+++ src/app/delictivo/registro-delictivo/registro-delictivo.component.spec.ts
@@ -1,19 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegistroDelictivoComponent } from './registro-delictivo.component';
 
 describe('RegistroDelictivoComponent', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   let component: RegistroDelictivoComponent;
   let fixture: ComponentFixture<RegistroDelictivoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegistroDelictivoComponent],
+      imports: [HttpClientTestingModule, FormsModule, ReactiveFormsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegistroDelictivoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should create', () => {
--- src/app/delictivo/services/delictivo.service.spec.ts
+++ src/app/delictivo/services/delictivo.service.spec.ts
@@ -1,13 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
 import { TestBed } from '@angular/core/testing';
 
 import { DelictivoService } from './delictivo.service';
 
 describe('DelictivoService', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   let service: DelictivoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(DelictivoService);
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
   it('should be created', () => {
